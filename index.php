1404743f751bfdbc3287f74526aa2ec6fcf59d5b
LucasDG1 <144344269+LucasDG1@users.noreply.github.com>
2024-05-30 10:30:24 +0200

    fdkjbjsfjnkml,

--- index.php
+++ index.php
@@ -10,17 +10,23 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+
+$id = isset($_GET['id']) ? intval($_GET['id']) : 1;
+
+$sqli_prepare = $conn->prepare("SELECT id, vraag, optieA, optieB, optieC, correct_antwoord, img FROM TheorieToetsCBR WHERE id = ?");
+$sqli_prepare->bind_param("i", $id);
+
+if ($sqli_prepare === false) {
+    echo mysqli_error($conn);
+} else {
+    if ($sqli_prepare->execute()) {
+        $sqli_prepare->bind_result($id, $vraag, $optieA, $optieB, $optieC, $correct, $img);
+        while($sqli_prepare->fetch()) {
 ?>
- <?php
-                    $sqli_prepare = $conn->prepare("SELECT id, vraag, optieA, optieB, optieC, correct_antwoord, img FROM TheorieToetsCBR WHERE id = " . $_GET["id"] . ";");
-                    if ($sqli_prepare === false) {
-                        echo mysqli_error($conn);
-                    } else{
-                        if ($sqli_prepare->execute()) {
-                            $sqli_prepare->bind_result($id, $vraag, $optieA, $optieB, $optieC, $correct, $img);
-                            while($sqli_prepare->fetch()){
-                                ?>
-                               <header>
+﻿
+
+    <header>
         <div class="topbar">
             <div class="topbar-flex">
                 <div class="cbr">CBR Theorie Toets</div>
@@ -30,32 +36,32 @@ if ($conn->connect_error) {
     <main>
         <div class="flex">
             <div class="img-box">
-                <img src="<?=$img?>" alt="logo" style="width: 700px; height: 700px; margin-left: 100px; margin-top: 50px;">
+                <img class="img"src="<?=$img?>" alt="logo">
             </div>
             <div id="vragen-container" class="hide">
                 <div class="titel"><?= $vraag ?></div>
-                    <div class="vraag-a">
-                        <div class="flex-box">
-                            <div class="a"><?=$optieA ?></div>
-                        </div>
+                <div class="vraag-a">
+                    <div class="flex-box">
+                        <div class="a"><?=$optieA ?></div>
                     </div>
-                    <div class="vraag-b">
-                        <div class="flex-box">                        
-                            <div class="b"><?= $optieB ?></div>
-                        </div>
+                </div>
+                <div class="vraag-b">
+                    <div class="flex-box">                        
+                        <div class="b"><?= $optieB ?></div>
                     </div>
-                    <div class="vraag-c">
-                        <div class="flex-box">                     
-                            <div class="c"><?=$optieC ?></div>
-                        </div>
+                </div>
+                <div class="vraag-c">
+                    <div class="flex-box">                     
+                        <div class="c"><?=$optieC ?></div>
                     </div>
-                    <div class="volgende-vraag">
-                        <div class="flex">
-                            <i class="fa fa-caret-square-o-left" aria-hidden="true" style="font-size: 100px; cursor: pointer;"></i>
-                            <div class="volgende-tekst">naar volgende vraag gaan</div>
-                            <i id="incrementDiv" class="fa fa-caret-square-o-right" aria-hidden="true" style="font-size: 100px; margin-right: 150px; cursor: pointer;"></i>
-                        </div>
+                </div>
+                <div class="volgende-vraag">
+                    <div class="flex">
+                        <i class="fa fa-caret-square-o-left" aria-hidden="true" style="font-size: 100px; cursor: pointer;"></i>
+                        <div class="volgende-tekst">naar volgende vraag gaan</div>
+                        <i id="incrementDiv" class="fa fa-caret-square-o-right" aria-hidden="true" style="font-size: 100px; margin-right: 150px; cursor: pointer;"></i>
                     </div>
+                </div>
             </div>
         </div>
     </main>
@@ -74,19 +80,29 @@ if ($conn->connect_error) {
     $sqli_prepare->close();
 ?>
     <script>
-        // Function to handle the click event
-        document.getElementById('incrementDiv').addEventListener('click', function() {
-            // Get the current ID from the URL or any other source
-            let currentId = parseInt(window.location.search.substring(1)); // Assuming the ID is in the URL as a query parameter
 
-            // Increment the ID
-            let nextId = currentId + 1;
 
-            // Update the URL to navigate to the next ID
-            window.location.href = 'index.php?id=' + nextId; // Replace 'page.php' with your actual page name
-        });
+    document.getElementById('incrementDiv').addEventListener('click', function() {
+        let currentId = new URLSearchParams(window.location.search).get('id'); 
+        if (!currentId) currentId = 1;
+        let nextId = parseInt(currentId) + 1;
+        window.location.href = 'index.php?id=' + nextId; 
+    });
+    document.getElementById('incrementDivmin').addEventListener('click', function() {
+        let currentId = new URLSearchParams(window.location.search).get('id'); 
+        if (!currentId) currentId = 1;
+        let nextId = parseInt(currentId) - 1;
+        window.location.href = 'index.php?id=' + nextId; 
+    });
     </script>
 
 
 </body>
 </html>
+<?php 
+        }
+    }
+    $sqli_prepare->close();
+}
+$conn->close();
+?>
