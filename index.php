4274c0469d781d99d39b55a7654e14f218c6201b
LucasDG1 <144344269+LucasDG1@users.noreply.github.com>
2024-05-30 10:18:59 +0200

    dfnfksmk

--- index.php
+++ index.php
@@ -10,17 +10,23 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+
+$id = isset($_GET['id']) ? intval($_GET['id']) : 1;
+
+$sqli_prepare = $conn->prepare("SELECT id, vraag, optieA, optieB, optieC, correct_antwoord, img FROM TheorieToetsCBR WHERE id = ?");
+$sqli_prepare->bind_param("i", $id);
+
+if ($sqli_prepare === false) {
+    echo mysqli_error($conn);
+} else {
+    if ($sqli_prepare->execute()) {
+        $sqli_prepare->bind_result($id, $vraag, $optieA, $optieB, $optieC, $correct, $img);
+        while($sqli_prepare->fetch()) {
 ?>
- <?php
-                    $sqli_prepare = $conn->prepare("SELECT id, vraag, optieA, optieB, optieC, correct_antwoord, img FROM TheorieToetsCBR WHERE id = " . $_GET["id"] . ";");
-                    if ($sqli_prepare === false) {
-                        echo mysqli_error($conn);
-                    } else{
-                        if ($sqli_prepare->execute()) {
-                            $sqli_prepare->bind_result($id, $vraag, $optieA, $optieB, $optieC, $correct, $img);
-                            while($sqli_prepare->fetch()){
-                                ?>
-                               <header>
+﻿
+
+    <header>
         <div class="topbar">
             <div class="topbar-flex">
                 <div class="cbr">CBR Theorie Toets</div>
@@ -30,7 +36,7 @@ if ($conn->connect_error) {
     <main>
         <div class="flex">
             <div class="img-box">
-                <img src="<?=$img?>" alt="logo" style="width: 700px; height: 700px; margin-left: 100px; margin-top: 50px;">
+                <img class="img"src="<?=$img?>" alt="logo">
             </div>
             <div id="vragen-container">
                 <div class="titel"><?= $vraag ?></div>
@@ -50,10 +56,10 @@ if ($conn->connect_error) {
                     </div>
                 </div>
                 <div class="volgende-vraag">
-                    <div class="flex">
-                        <i class="fa fa-caret-square-o-left" aria-hidden="true" style="font-size: 100px; cursor: pointer;"></i>
+                    <div class="flex-kies">
+                        <i id="incrementDivmin" class="fa fa-caret-square-o-left" aria-hidden="true"></i>
                         <div class="volgende-tekst">naar volgende vraag gaan</div>
-                        <i id="incrementDiv" class="fa fa-caret-square-o-right" aria-hidden="true" style="font-size: 100px; margin-right: 150px; cursor: pointer;"></i>
+                        <i id="incrementDiv" class="fa fa-caret-square-o-right" aria-hidden="true" ></i>
                     </div>
                 </div>
             </div>
@@ -68,17 +74,20 @@ if ($conn->connect_error) {
         </div>
     </footer>
     <script>
-        // Function to handle the click event
-        document.getElementById('incrementDiv').addEventListener('click', function() {
-            // Get the current ID from the URL or any other source
-            let currentId = parseInt(window.location.search.substring(1)); // Assuming the ID is in the URL as a query parameter
 
-            // Increment the ID
-            let nextId = currentId + 1;
 
-            // Update the URL to navigate to the next ID
-            window.location.href = 'index.php?id=' + nextId; // Replace 'page.php' with your actual page name
-        });
+    document.getElementById('incrementDiv').addEventListener('click', function() {
+        let currentId = new URLSearchParams(window.location.search).get('id'); 
+        if (!currentId) currentId = 1;
+        let nextId = parseInt(currentId) + 1;
+        window.location.href = 'index.php?id=' + nextId; 
+    });
+    document.getElementById('incrementDivmin').addEventListener('click', function() {
+        let currentId = new URLSearchParams(window.location.search).get('id'); 
+        if (!currentId) currentId = 1;
+        let nextId = parseInt(currentId) - 1;
+        window.location.href = 'index.php?id=' + nextId; 
+    });
     </script>
 </body>
 </html>
